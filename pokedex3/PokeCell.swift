3ba4d2d5fb946a12a5362e53a8ce905033cc7506
margibs <margibs_hbb@yahoo.com>
2016-09-22 14:47:44 +0800

    update

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,35 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by margibs on 21/09/2016.
+//  Copyright © 2016 margibs. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+        
+    }
+    
+    func configureCell(_ pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+}
